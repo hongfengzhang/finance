9ab4ff7e56bb046801db91c0bea914b9fbea665e
493671072@qq.com <493671072@qq.com>
2017-12-06 20:05:51 +0800

    级联获取地区、地区下支行列表接口

--- datalayer/buyrecord/src/main/java/com/waben/stock/datalayer/buyrecord/BuyRecordApplication.java
+++ datalayer/buyrecord/src/main/java/com/waben/stock/datalayer/buyrecord/BuyRecordApplication.java
@@ -3,10 +3,17 @@ package com.waben.stock.datalayer.buyrecord;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+//服务发现客户端
 @EnableDiscoveryClient
+//服务调用
+@EnableFeignClients
+//断路器
+@EnableHystrix
 // 扫描包
 @ComponentScan(basePackages = { "com.waben.stock" })
 public class BuyRecordApplication {
