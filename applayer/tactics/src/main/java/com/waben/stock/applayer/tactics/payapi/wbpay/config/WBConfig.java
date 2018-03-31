a10edfaba6aed5ab1f6602f8fa73f6fd3649e364
lizhenhua <lizhenhua@youguwang.com.cn>
2018-03-31 14:52:18 +0800

    '加入收银台支付'

--- applayer/tactics/src/main/java/com/waben/stock/applayer/tactics/payapi/wbpay/config/WBConfig.java
+++ applayer/tactics/src/main/java/com/waben/stock/applayer/tactics/payapi/wbpay/config/WBConfig.java
@@ -1,10 +1,14 @@
 package com.waben.stock.applayer.tactics.payapi.wbpay.config;
 
 public class WBConfig {
-    public static final String merchantNo = "888888888";
-    public static final String key = "134fb0bf3bdd54ee9098f4cbc4351b9a";
+    public static final String merchantNo = "351647028";
+    public static final String key = "e69fb42096afcf22242264a7d482a6eb";
     public static final String tradeType = "quick_bank";
-    public static final String notifyUrl = "";
-    public static final String frontUrl = "";
+    public static final String notifyUrl = "https://qqtong.club/tactics/quickpay/wbcallback";
+    public static final String frontUrl = "https://qqtong.club/tactics/quickpay/wbreturn";
+//    public static final String notifyUrl = "http://yyqr3z.natappfree.cc/quickpay/wbcallback";
+//    public static final String frontUrl = "http://yyqr3z.natappfree.cc/quickpay/wbreturn";
     public static final String quick_bank_url = "http://b.waben.com.cn/api/quick/bankpay";
+    public static final String  domain= "http://b.waben.com.cn";
+    public static final String platform_url ="http://b.waben.com.cn/api/platform/pay";
 }
