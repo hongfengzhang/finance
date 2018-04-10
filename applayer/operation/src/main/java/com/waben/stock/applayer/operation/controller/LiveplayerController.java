f7c95d2c522a4c911d769cccafbab2eeecf48d5f
493671072@qq.com <493671072@qq.com>
2018-04-10 21:20:59 +0800

    直播接口

--- applayer/operation/src/main/java/com/waben/stock/applayer/operation/controller/LiveplayerController.java
+++ applayer/operation/src/main/java/com/waben/stock/applayer/operation/controller/LiveplayerController.java
@@ -3,13 +3,22 @@ package com.waben.stock.applayer.operation.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.waben.stock.interfaces.commonapi.netease.bean.NeteaseChannellistRet;
+import com.waben.stock.interfaces.pojo.Response;
+import com.waben.stock.interfaces.pojo.query.PageInfo;
+
 @Controller
 @RequestMapping("/liveplayer")
 public class LiveplayerController {
-	
+
 	@RequestMapping("/index")
 	public String index() {
 		return "manage/liveplayer/index";
 	}
-	
+
+	public Response<PageInfo<NeteaseChannellistRet>> livePlayerList(int page, int size) {
+		
+		return null;
+	}
+
 }
