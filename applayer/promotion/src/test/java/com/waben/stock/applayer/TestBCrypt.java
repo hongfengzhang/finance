aa7ea4afb5bd4b769ba291225437b8b256de8e48
493671072@qq.com <493671072@qq.com>
2018-04-19 10:06:37 +0800

    后台运营平台增加修改密码的功能

--- applayer/promotion/src/test/java/com/waben/stock/applayer/TestBCrypt.java
+++ applayer/promotion/src/test/java/com/waben/stock/applayer/TestBCrypt.java
@@ -5,7 +5,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class TestBCrypt {
 	
-	public static void testMain(String[] args) {
+	public static void main(String[] args) {
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 		System.out.println(encoder.encode("wangbei"));
 	}
