6c0d1ab4c5cd94e19bc1a94423469d74d9597507
zzw999 <1372079788@qq.com>
2018-05-02 14:27:17 +0800

    自动行权日如果是非工作日或节假日则在前一个工作日行权

--- risk/src/main/java/com/waben/stock/risk/schedule/job/StockOptionDueTreatmentJob.java
+++ risk/src/main/java/com/waben/stock/risk/schedule/job/StockOptionDueTreatmentJob.java
@@ -1,17 +1,17 @@
 package com.waben.stock.risk.schedule.job;
 
+import com.waben.stock.interfaces.constants.HolidayConstant;
 import com.waben.stock.interfaces.dto.stockoption.StockOptionTradeDto;
 import com.waben.stock.interfaces.enums.StockOptionTradeState;
 import com.waben.stock.interfaces.util.JacksonUtil;
 import com.waben.stock.risk.business.StockOptionTradeBusiness;
-import com.waben.stock.risk.container.StockApplyEntrustSellOutContainer;
 import com.waben.stock.risk.warpper.ApplicationContextBeanFactory;
 import org.quartz.*;
+import org.quartz.impl.calendar.WeeklyCalendar;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -26,7 +26,7 @@ public class StockOptionDueTreatmentJob implements InterruptableJob {
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
         SimpleDateFormat fmt = new SimpleDateFormat("yyyyMMdd");
-
+        SimpleDateFormat hd = new SimpleDateFormat("yyyy-MM-dd");
         List<StockOptionTradeDto> stockOptionTradeDtos = null;
         boolean flag = false;
         while(!flag) {
@@ -37,9 +37,21 @@ public class StockOptionDueTreatmentJob implements InterruptableJob {
                 logger.info("错误信息：{}",e.getMessage());
             }
         }
+        WeeklyCalendar workDay = new WeeklyCalendar();
+        Calendar calendar = Calendar.getInstance();
+
         for (StockOptionTradeDto stockOptionTradeDto : stockOptionTradeDtos) {
             Date expireTime = stockOptionTradeDto.getExpireTime();
             Date currentTime = new Date();
+            boolean isWork = workDay.isTimeIncluded(expireTime.getTime());//是否是工作日
+            int isHoliyday = HolidayConstant.holiyday_2018.indexOf(hd.format(expireTime));//是否是节假日
+            while (isHoliyday>0||!isWork) {
+                calendar.setTime(expireTime);
+                calendar.add(Calendar.DATE, -1);
+                expireTime = calendar.getTime();
+                isHoliyday = HolidayConstant.holiyday_2018.indexOf(hd.format(expireTime));
+                isWork = workDay.isTimeIncluded(expireTime.getTime());
+            }
             flag = true;
             if(fmt.format(expireTime).equals(fmt.format(currentTime))) {
                 while (flag) {
