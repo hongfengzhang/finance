2d597c9b14dc3d57de18de496287d8962c9a7f60
493671072@qq.com <493671072@qq.com>
2018-05-11 10:04:12 +0800

    fix bug

--- interfaces/src/main/java/com/waben/stock/interfaces/pojo/query/organization/TradingFowQuery.java
+++ interfaces/src/main/java/com/waben/stock/interfaces/pojo/query/organization/TradingFowQuery.java
@@ -2,7 +2,7 @@ package com.waben.stock.interfaces.pojo.query.organization;
 
 import java.util.Date;
 
-import com.waben.stock.interfaces.enums.CapitalFlowType;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.waben.stock.interfaces.pojo.query.PageAndSortQuery;
 
 /**
@@ -21,101 +21,36 @@ public class TradingFowQuery extends PageAndSortQuery {
 	/**
 	 * 客户姓名
 	 */
-	private String customerName;
+	private String publisherName;
 
 	/**
 	 * 交易账号
 	 */
-	private String tradingNumber;
-
+	private String publisherPhone;
 	/**
-	 * 交易开始时间
+	 * 交易时间-查询开始时间
 	 */
-	private Date occurrenceTimeStart;
-
+	@JsonFormat(timezone = "GMT+8", pattern = "yyyy-MM-dd HH:mm:ss")
+	private Date startTime;
 	/**
-	 * 交易结束时间
+	 * 交易时间-查询结束时间
 	 */
-	private Date occurrenceTimeEnd;
-
+	@JsonFormat(timezone = "GMT+8", pattern = "yyyy-MM-dd HH:mm:ss")
+	private Date endTime;
 	/**
 	 * 业务类型
 	 */
-	private CapitalFlowType type;
+	private String types;
 
 	/**
 	 * 股票代码
 	 */
-	private String stockCode;
+	private String stockCodeOrName;
 
 	/**
 	 * 所属代理商代码/名称
 	 */
-	private String agentCodeName;
-
-	/**
-	 * 查询类型
-	 * <p>
-	 * 1 平台登录、查询所有数据，2 代理商登录、查询自己数据+直属代理商数据
-	 * </p>
-	 */
-//	private Integer queryType;
-
-	public String getCustomerName() {
-		return customerName;
-	}
-
-	public void setCustomerName(String customerName) {
-		this.customerName = customerName;
-	}
-
-	public String getTradingNumber() {
-		return tradingNumber;
-	}
-
-	public void setTradingNumber(String tradingNumber) {
-		this.tradingNumber = tradingNumber;
-	}
-
-	public Date getOccurrenceTimeStart() {
-		return occurrenceTimeStart;
-	}
-
-	public void setOccurrenceTimeStart(Date occurrenceTimeStart) {
-		this.occurrenceTimeStart = occurrenceTimeStart;
-	}
-
-	public Date getOccurrenceTimeEnd() {
-		return occurrenceTimeEnd;
-	}
-
-	public void setOccurrenceTimeEnd(Date occurrenceTimeEnd) {
-		this.occurrenceTimeEnd = occurrenceTimeEnd;
-	}
-
-	public CapitalFlowType getType() {
-		return type;
-	}
-
-	public void setType(CapitalFlowType type) {
-		this.type = type;
-	}
-
-	public String getStockCode() {
-		return stockCode;
-	}
-
-	public void setStockCode(String stockCode) {
-		this.stockCode = stockCode;
-	}
-
-	public String getAgentCodeName() {
-		return agentCodeName;
-	}
-
-	public void setAgentCodeName(String agentCodeName) {
-		this.agentCodeName = agentCodeName;
-	}
+	private String orgCodeOrName;
 
 	public Long getCurrentOrgId() {
 		return currentOrgId;
@@ -123,6 +58,62 @@ public class TradingFowQuery extends PageAndSortQuery {
 
 	public void setCurrentOrgId(Long currentOrgId) {
 		this.currentOrgId = currentOrgId;
+	}
+
+	public String getPublisherName() {
+		return publisherName;
+	}
+
+	public void setPublisherName(String publisherName) {
+		this.publisherName = publisherName;
+	}
+
+	public String getPublisherPhone() {
+		return publisherPhone;
+	}
+
+	public void setPublisherPhone(String publisherPhone) {
+		this.publisherPhone = publisherPhone;
+	}
+
+	public Date getStartTime() {
+		return startTime;
+	}
+
+	public void setStartTime(Date startTime) {
+		this.startTime = startTime;
+	}
+
+	public Date getEndTime() {
+		return endTime;
+	}
+
+	public void setEndTime(Date endTime) {
+		this.endTime = endTime;
+	}
+
+	public String getTypes() {
+		return types;
+	}
+
+	public void setTypes(String types) {
+		this.types = types;
+	}
+
+	public String getStockCodeOrName() {
+		return stockCodeOrName;
+	}
+
+	public void setStockCodeOrName(String stockCodeOrName) {
+		this.stockCodeOrName = stockCodeOrName;
+	}
+
+	public String getOrgCodeOrName() {
+		return orgCodeOrName;
+	}
+
+	public void setOrgCodeOrName(String orgCodeOrName) {
+		this.orgCodeOrName = orgCodeOrName;
 	}
 
 }
