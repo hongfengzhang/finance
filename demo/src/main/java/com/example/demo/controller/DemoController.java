524d598fe49dd506dab0f7842e3fd72e53520e3e
493671072@qq.com <493671072@qq.com>
2018-06-07 15:08:10 +0800

    bug

--- demo/src/main/java/com/example/demo/controller/DemoController.java
+++ demo/src/main/java/com/example/demo/controller/DemoController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoController {
 
-	@GetMapping("hello")
+	@GetMapping("")
 	private String demo(){
 		return "HELLO WORD THREE";
 	}
