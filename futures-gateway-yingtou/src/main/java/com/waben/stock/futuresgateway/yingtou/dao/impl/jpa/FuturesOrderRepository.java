2553d0002927f5823226c2c8110698c8123bd4d2
493671072@qq.com <493671072@qq.com>
2018-06-08 14:13:17 +0800

    项目重命名

---
+++ futures-gateway-yingtou/src/main/java/com/waben/stock/futuresgateway/yingtou/dao/impl/jpa/FuturesOrderRepository.java
@@ -0,0 +1,33 @@
+package com.waben.stock.futuresgateway.yingtou.dao.impl.jpa;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.Repository;
+
+import com.waben.stock.futuresgateway.yingtou.entity.FuturesOrder;
+
+/**
+ * 期货订单 Repository
+ * 
+ * @author luomengan
+ *
+ */
+public interface FuturesOrderRepository extends Repository<FuturesOrder, Long> {
+
+	FuturesOrder save(FuturesOrder futuresOrder);
+
+	void delete(Long id);
+
+	Page<FuturesOrder> findAll(Pageable pageable);
+	
+	List<FuturesOrder> findAll();
+
+	FuturesOrder findById(Long id);
+
+	FuturesOrder findByTwsOrderId(int twsOrderId);
+
+	FuturesOrder findByDomainAndOuterOrderId(String domain, Long outerOrderId);
+	
+}
