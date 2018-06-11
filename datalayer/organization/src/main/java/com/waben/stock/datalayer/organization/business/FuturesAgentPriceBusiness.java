40a2e442b447b8f04deaf5c2a04696d470bacb3d
493671072@qq.com <493671072@qq.com>
2018-06-11 20:30:57 +0800

    a

--- datalayer/organization/src/main/java/com/waben/stock/datalayer/organization/business/FuturesAgentPriceBusiness.java
+++ datalayer/organization/src/main/java/com/waben/stock/datalayer/organization/business/FuturesAgentPriceBusiness.java
@@ -5,9 +5,11 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import com.waben.stock.interfaces.dto.futures.FuturesContractDto;
+import com.waben.stock.interfaces.dto.organization.OrganizationDto;
 import com.waben.stock.interfaces.exception.ServiceException;
 import com.waben.stock.interfaces.pojo.Response;
 import com.waben.stock.interfaces.service.futures.FuturesContractInterface;
+import com.waben.stock.interfaces.service.organization.OrganizationInterface;
 
 /**
  * 期货代理价格 Business
@@ -22,8 +24,20 @@ public class FuturesAgentPriceBusiness {
 	@Qualifier("futurescontractInterface")
 	private FuturesContractInterface futuresContractInterface;
 
+	@Autowired
+	@Qualifier("futurescontractInterface")
+	private OrganizationInterface organizationInterface;
+
 	public FuturesContractDto getFuturesContractDto(Long contractId) {
 		Response<FuturesContractDto> response = futuresContractInterface.findByContractId(contractId);
+		if (response.getCode().equals("200")) {
+			return response.getResult();
+		}
+		throw new ServiceException(response.getCode());
+	}
+
+	public OrganizationDto fetchByOrgId(Long id) {
+		Response<OrganizationDto> response = organizationInterface.fetchByOrgId(id);
 		if (response.getCode().equals("200")) {
 			return response.getResult();
 		}
