447128bc9575e30ca1960ea831319d4e81d87eb9
493671072@qq.com <493671072@qq.com>
2018-06-13 09:31:13 +0800

    fix

--- futures-gateway-yisheng/src/main/java/com/waben/stock/futuresgateway/yisheng/esapi/schedule/QuoteMinuteKGroupSchedule.java
+++ futures-gateway-yisheng/src/main/java/com/waben/stock/futuresgateway/yisheng/esapi/schedule/QuoteMinuteKGroupSchedule.java
@@ -1,5 +1,8 @@
 package com.waben.stock.futuresgateway.yisheng.esapi.schedule;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -17,9 +20,10 @@ public class QuoteMinuteKGroupSchedule {
 	/**
 	 * 每分钟计算上一分钟的分钟K
 	 */
-	@Scheduled(cron = "0 20 5/1 * * ?")
+	@Scheduled(cron = "0 37 23 0/1 * ?")
 	public void run() {
-		
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		System.out.println(sdf.format(new Date()));
 	}
 
 }
